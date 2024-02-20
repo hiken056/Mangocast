e1684647710bbe53b873188e1e1594125c0004cc
hiken056 <stefanrusu60@gmail.com>
2024-02-20 16:00:37 +0200

    env not working

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+
+const URI = process.env.MONGO_URI as string;
+
 mongoose
   .connect('mongodb://127.0.0.1:27017/podcastApp')
   .then(() => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import 'dotenv/config'
 import "./db";
+
+
 
 const app = express();
 
-const PORT = 8989
+const PORT = process.env.PORT || 8989
 
 app.listen(PORT, () => {
   console.log("port is listening on port " + PORT);
