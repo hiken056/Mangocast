1138e92f78124b80152a784557bf4ea2c2e2a6aa
hiken056 <stefanrusu60@gmail.com>
2024-02-22 00:06:55 +0200

    tsc-alias

---
+++ server/dist/db/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+mongoose_1.default.set("strictQuery", true);
+mongoose_1.default
+    .connect('mongodb://127.0.0.1:27017/podcastApp')
+    .then(() => {
+    console.log("connection succesful to the server");
+})
+    .catch((err) => {
+    console.log("connection failed to the server", err);
+});
