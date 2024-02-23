5c08f35f4212ef63390f2570db67d573ebcf9031
hiken056 <stefanrusu60@gmail.com>
2024-02-23 22:59:03 +0200

    user modules

---
+++ server/src/models/user.ts
@@ -0,0 +1,53 @@
+//interface
+
+import { ObjectId, Schema } from "mongoose";
+
+interface userDocument {
+    name: string;
+    email: string;
+    password: string;
+    verified: boolean;
+    avatar?: {url: string; publicId: string};
+    tokens: string[];
+    favorites: ObjectId[];
+    followers: ObjectId[];
+    followings: ObjectId[];
+}
+
+const userSchema = new Schema<userDocument>({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: Object,
+        url: String,
+        publicId: String
+    },
+    verified: {
+        type: Boolean,
+        default: false
+    },
+    favorites: [{
+        type: Schema.Types.ObjectId,
+        ref: "Audio", 
+    }],
+    followers: [{
+        type: Schema.Types.ObjectId, 
+    }],
+    followings: [{
+        type: Schema.Types.ObjectId, 
+    }],
+    tokens: [String],
+})
