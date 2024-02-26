7b524c5d5d321366e11f7955817689d02aa44c68
hiken056 <stefanrusu60@gmail.com>
2024-02-26 15:29:22 +0200

    nmp package yup

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -1,9 +1,22 @@
+import { CreateUser } from '#/@types/user';
 import User from '#/models/user';
+import { error } from 'console';
 import {Router} from 'express'
 
 const router = Router();
 
-router.post('/create', async (req, res) => {
+router.post('/create',
+
+//user validation for name
+// (req, res, next) => {
+//     const {email, password, name} = req.body;
+//     if ( !name.trim() ) return res.json({error: "Name is missing!"});    
+//     if ( name.lenght < 3 ) return res.json({error: "Invalid name!"});    
+    
+//     next();   
+// }
+
+ async (req: CreateUser, res) => {
     const {email, password, name} = req.body;
     
     const user = await User.create({name, email, password}) 
