6840f9024707d20d376bd0f34f7228da1f17deb5
hiken056 <stefanrusu60@gmail.com>
2024-02-26 15:42:30 +0200

    validation with yup for name, email and password

---
+++ server/src/utils/validationSchema.ts
@@ -0,0 +1,23 @@
+import * as yup from "yup";
+
+export const CreateUserSchema = yup.object().shape({
+  name: yup
+    .string()
+    .trim()
+    .required("Name is missing!")
+    .min(3, "Name is too short!")
+    .max(20, "Name is too long!"),
+  emai: yup.string().required("Email is missing!").email("Invalid email!"),
+  password: yup
+    .string()
+    .trim()
+    .required("Passward is missing!")
+    .min(8, "Password is too short!")
+    .matches(
+      /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+      "Password must contain at least 8 characters, one uppercase, one number and one special case character"
+    ),
+});
+
+
+// CreateUserSchema.validate()
