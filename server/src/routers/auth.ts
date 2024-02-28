f1af3cdd0bff1b6517f36ac9db032efb623b41cb
hiken056 <stefanrusu60@gmail.com>
2024-02-28 21:48:48 +0200

    status codes and clean auth.ts

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -1,8 +1,6 @@
-import { CreateUser } from "#/@types/user";
+import { create } from "#/contollers/user";
 import { validate } from "#/middleware/validator";
-import User from "#/models/user";
 import { CreateUserSchema } from "#/utils/validationSchema";
-import { error } from "console";
 import { Router } from "express";
 
 const router = Router();
@@ -20,16 +18,7 @@ router.post(
   // }
 
   validate(CreateUserSchema),
-
-  async (req: CreateUser, res) => {
-    const { email, password, name } = req.body;
-
-    CreateUserSchema.validate({ email, password, name }).catch((error) => {console.log(error);});
-    
-
-   const user = await User.create({ name, email, password });
-   res.json({ user });
-  }
+  create
 );
 
 export default router;
