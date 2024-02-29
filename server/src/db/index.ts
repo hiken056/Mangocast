2f045480364a2a12b06e82edf697b78ad7f3a23e
hiken056 <stefanrusu60@gmail.com>
2024-02-29 23:14:35 +0200

    mor cu .env

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,15 +1,14 @@
-// Manually set the environment variable
-process.env.MONGO_URI = 'mongodb://127.0.0.1:27017/podcastApp';
 
-// Log the value to check if it's set correctly
-console.log(process.env.MONGO_URI);
-
-// Continue with your code
 import mongoose from "mongoose";
+
+require('dotenv').config();
+console.log(process.env.MONGO_URI); // Check if MONGO_URI is defined
+
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect( MONGO_URI as string )
   .then(() => {
     console.log("connection successful to the server");
   })
--- server/src/utils/variables.ts
+++ server/src/utils/variables.ts
@@ -1,3 +1,7 @@
 const { env } = process as { env: { [key: string]: string } };
 
-export const { MONGO_URI, MAILTRAP_USER, MAILTRAP_PASS } = env;
+export const MONGO_URI = process.env.MONGO_URI;
+
+export const  MAILTRAP_PASS = process.env.MAILTRAP_PASS;
+
+export const  MAILTRAP_USER = process.env.MAILTRAP_USER;
