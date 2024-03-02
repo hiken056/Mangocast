a7b5b973fec585fc2e3f1a0ef4943109780e7abb
hiken056 <stefanrusu60@gmail.com>
2024-03-02 10:53:34 +0200

    MERGE ENV

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,14 +1,13 @@
 
+import { MONGO_URI } from "#/utils/variables";
 import mongoose from "mongoose";
 
 require('dotenv').config();
-console.log(process.env.MONGO_URI); // Check if MONGO_URI is defined
 
-const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.set("strictQuery", true);
 mongoose
-  .connect( MONGO_URI as string )
+  .connect(MONGO_URI)
   .then(() => {
     console.log("connection successful to the server");
   })
--- server/src/utils/variables.ts
+++ server/src/utils/variables.ts
@@ -1,7 +1,7 @@
 const { env } = process as { env: { [key: string]: string } };
 
-export const MONGO_URI = process.env.MONGO_URI;
+export const MONGO_URI = process.env.MONGO_URI as string;
 
-export const  MAILTRAP_PASS = process.env.MAILTRAP_PASS;
+export const  MAILTRAP_PASS = process.env.MAILTRAP_PASS as string;
 
-export const  MAILTRAP_USER = process.env.MAILTRAP_USER;
+export const  MAILTRAP_USER = process.env.MAILTRAP_USER as string;
