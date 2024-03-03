c77de65c1553cecf4a9ef02fd703f6524eebabe2
hiken056 <stefanrusu60@gmail.com>
2024-03-03 14:16:19 +0200

    user and pass not working

--- server/src/contollers/user.ts
+++ server/src/contollers/user.ts
@@ -1,9 +1,9 @@
 import { RequestHandler } from "express";
 import nodemailer from "nodemailer";
 
+import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
 import { CreateUser } from "#/@types/user";
 import User from "#/models/user";
-import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
