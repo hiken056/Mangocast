3e62428b8cdabb5190639429ef546134b72a122a
hiken056 <stefanrusu60@gmail.com>
2024-03-04 17:28:00 +0200

    hashed password

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,57 +1,84 @@
 //interface
 
+import { compare, hash } from "bcrypt";
 import { Model, ObjectId, Schema, model } from "mongoose";
 
 interface userDocument {
-    name: string;
-    email: string;
-    password: string;
-    verified: boolean;
-    avatar?: {url: string; publicId: string};
-    tokens: string[];
-    favorites: ObjectId[];
-    followers: ObjectId[];
-    followings: ObjectId[];
+  name: string;
+  email: string;
+  password: string;
+  verified: boolean;
+  avatar?: { url: string; publicId: string };
+  tokens: string[];
+  favorites: ObjectId[];
+  followers: ObjectId[];
+  followings: ObjectId[];
 }
 
-const userSchema = new Schema<userDocument>({
+interface Methods {
+  comparePassword(password: string): Promise<boolean>;
+}
+
+const userSchema = new Schema<userDocument, {}, Methods>(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     avatar: {
-        type: Object,
-        url: String,
-        publicId: String
+      type: Object,
+      url: String,
+      publicId: String,
     },
     verified: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-    favorites: [{
+    favorites: [
+      {
         type: Schema.Types.ObjectId,
-        ref: "Audio", 
-    }],
-    followers: [{
-        type: Schema.Types.ObjectId,
-        ref: "User", 
-    }],
-    followings: [{
+        ref: "Audio",
+      },
+    ],
+    followers: [
+      {
         type: Schema.Types.ObjectId,
         ref: "User",
-    }],
+      },
+    ],
+    followings: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
     tokens: [String],
-}, {timestamps: true});
+  },
+  { timestamps: true }
+);
 
-export default model("User", userSchema) as Model<userDocument>
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await hash(this.password, 10);
+  }
+  next();
+});
+
+// Define method to compare tokens
+userSchema.methods.comparePassword = async function (password) {
+  const result = await compare(password, this.password);
+  return result;
+};
+
+export default model("User", userSchema) as Model<userDocument, {}, Methods>;
