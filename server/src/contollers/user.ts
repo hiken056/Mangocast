4dfe38be3e2f2bc708a35510f6f938289a8368f5
hiken056 <stefanrusu60@gmail.com>
2024-03-04 17:05:34 +0200

    email frumos

--- server/src/contollers/user.ts
+++ server/src/contollers/user.ts
@@ -1,15 +1,16 @@
 import { RequestHandler } from "express";
 import nodemailer from "nodemailer";
+import path from 'path';
 
 import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
 import { CreateUser } from "#/@types/user";
 import User from "#/models/user";
 import emailVerificationToken from "#/models/emailVerificationToken";
 import { generateToken } from "#/utils/helper";
+import { generateTemplate } from "#/mail/template";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
-
 
   const user = await User.create({ name, email, password });
 
@@ -32,10 +33,34 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
     token,
   });
 
+  const welcomeMessage = `Hi, ${name}, welcome to Mangocast! There are so many things we do for our verified users. 
+  Use the given token to verify you email.`;
+
   transport.sendMail({
     to: user.email,
     from: "auth@myapp.com",
-    html: `<h1>Your verification token is: ${token}</h1>`
+    subject: "Welcome message",
+    html: generateTemplate({
+      title: "Welcome to Mangocast!",
+      message: welcomeMessage,
+      logo: "cid:logo",
+      banner: "cid:welcome",
+      link: "#",
+      btnTitle: token
+    }),
+    attachments: [
+      {
+        filename: "logo.png",
+        path: path.join(__dirname, "../mail/logo.png"),
+        cid: "logo"
+      },
+      {
+        filename: "welcome.png",
+        path: path.join(__dirname, "../mail/welcome.png"),
+        cid: "welcome"
+      },
+      
+    ]
   });
 
   res.status(201).json({ user });
