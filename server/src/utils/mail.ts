a55fe5fa17026114610ae813b53292badcbf4114
hiken056 <stefanrusu60@gmail.com>
2024-03-29 23:15:47 +0200

    changes

--- server/src/utils/mail.ts
+++ server/src/utils/mail.ts
@@ -122,7 +122,7 @@ export const sendPassResetSuccessEmail = async (
       message,
       logo: "cid:logo",
       banner: "cid:forget_password",
-      link: SIGN_IN_URL,
+      link: SIGN_IN_URL as string,
       btnTitle: "Log in",
     }),
     attachments: [
