a95c115f7cbe046d40745443e4e43f956bfb2d29
hiken056 <stefanrusu60@gmail.com>
2024-03-31 22:29:17 +0300

    is auth

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -9,13 +9,16 @@ import {
 } from "#/controllers/user";
 import { isValidPassResetToken } from "#/middleware/auth";
 import { validate } from "#/middleware/validator";
+import User from "#/models/user";
 import {
   CreateUserSchema,
   SignInValidationSchema,
   TokenAndIDValidation,
   UpdatePasswordSchema,
 } from "#/utils/validationSchema";
+import { JWT_SECRET } from "#/utils/variables";
 import { Router } from "express";
+import { Jwt, JwtPayload, verify } from "jsonwebtoken";
 
 const router = Router();
 
@@ -36,5 +39,28 @@ router.post(
   updatePassword
 );
 router.post("/sign-in", validate(SignInValidationSchema), signIn);
+router.get("/is-auth", async (req, res) => {
+  const { authorization } = req.headers;
+  const token = authorization?.split("Bearer ")[1];
+  if (!token) return res.status(403).json({ error: "Unauthorized request!" });
+
+  const payload = verify(token, JWT_SECRET as string) as JwtPayload;
+  const id = payload.userId;
+
+  const user = await User.findById(id);
+
+  if (!user) return res.status(403).json({ error: "Unauthorized request!" });
+
+  res.json({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    verified: user.verified,
+    avatar: user.avatar?.url,
+    followers: user.followers.length,
+    folowings: user.followings.length,
+  });
+  res.json({ ok: true });
+});
 
 export default router;
